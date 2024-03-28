53ab2fef369d8ae890f0c37debc6e191c98b8388
Martino Facchin <m.facchin@arduino.cc>
2024-03-28 10:57:57 +0100

    Add leds test to Ecnoder test

--- examples/EncoderSetter/EncoderSetter.ino
+++ examples/EncoderSetter/EncoderSetter.ino
@@ -1,18 +1,25 @@
 #include "Modulino.h"
 
 ModulinoKnob encoder;
+ModulinoPixels leds;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Modulino.begin();
   encoder.begin();
+  leds.begin();
 }
 
 void loop() {
   int value = encoder.get();
-  if (value > 7 || value < 0) {
+  if (value > 100 || value < 0) {
     encoder.set(0);
   }
+  value = encoder.get();
   Serial.println(value);
+  leds.set(1, RED, value);
+  leds.set(2, GREEN, value);
+  leds.set(3, BLUE, value);
+  leds.show();
 }
